68a2ddd41b658ac66e79725d9f4c4e9cc2c595a2
alemonacoo <ale.monaco@outlook.com>
2022-09-27 16:16:52 +0200

    adding axios config

--- src/components/main/MainComponent.vue
+++ src/components/main/MainComponent.vue
@@ -1,18 +1,52 @@
 <template>
-<div>
+<main>
 
-</div>
+</main>
 </template>
 
 <script>
+import axios from 'axios';
+
+
 
 export default {
-    name: "MainComponent"
+    name: "MainComponent",
+    data(){
+        return{
+            url: 'https://flynn.boolean.careers/exercises/api/array/music',
+            albums: []
+        }
+    },
+    created(){
+        this.getAlbumsData();
+
+    },
+    methods:{
+        getAlbumsData(){
+            axios.get(this.url)
+            .then((response) =>{
+                if (this.idResponseOk(response)){
+                    console.log(response.data);
+                    response.data = this.albums;
+                }
+                
+            })
+        },
+        idResponseOk({status}){
+            return status === 200;
+        }
+    },
+    
 }
 </script>
 
 <style lang="scss" scoped>
-@import '@/assets/main.scss'
-
+@import '@/assets/main.scss';
+main{
+    height: 93vh;
+    width: 100%;
+    background-color: $app-dark;
+    padding: 5% 10%;
+}
 
 </style>
